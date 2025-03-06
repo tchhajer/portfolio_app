4694a9fa83f34b2498cba2ffe6c1f8f91445a5b2
vijayrampatel <vpatel57@wisc.edu>
2025-03-05 21:56:37 -0600

    questions for cache

---
+++ pre_defined_answers.py
@@ -0,0 +1,41 @@
+all_projects = f'''Sure! Here are some of my projects:
+
+- Redfin Housing Data Pipeline (2024):
+  - Built ETL pipeline using AWS, Snowflake, and Airflow to process 39M housing records.
+  - Created Tableau dashboard for 10-year property price analysis.
+
+- Real-time Data Pipeline (2024):
+  - Developed Kafka/Docker pipeline processing 10M+ daily streaming data points.
+  - Enhanced processing efficiency by 40% with advanced filtering and transformation.
+  - Generated insights on app usage and device distribution for 1M+ users.
+
+- Fraud Transaction Detection (2023):
+  - Built models (Logistic Regression, Decision Trees, Gradient Boost) on 7M+ rows.
+  - Improved accuracy by 15% and reduced optimization time by 30% through hyperparameter tuning.
+
+Let me know if you need more details!
+'''
+
+all_certifications = f'''Sure! Here are some of my certifications:
+1) AWS Certified Data Engineer - Associate (2024)
+2) SAS Certified Specialist: Base Programming (2022)
+3) Fundamentals of the Databricks Lakehouse Platform (2025)
+'''
+
+all_qualifications = f'''1) <b>Masters in Computer and Information Science</b> - University of Wisconsin-Madison (2023-2025)  
+    - GPA: 3.9/4.0
+    - Specialization: Machine Learning & Analytics
+    - Coursework: Deep Learning, Data Visualization, Database Design, Cloud Technology, Data Management for Data Science
+
+2) <b>Bachelor of Technology in Electronics Engineering</b> - University of Mumbai (2018-2022)  
+    - Graduated With Distinction (Top 5% cohort)  
+    - Departmental Award for Academic Excellence
+'''
+
+all_experience = '''Below is my work experience history:
+1) Data Science Engineer Intern- Field Day Lab (October 2025 - Present)
+2) Data Analyst Intern - Universties of Wisconsin System (August 2024 - Present)
+3) Research Assistant - University of Wisconsin-Madison (January 2024 - Present)
+4) Research Software Developer Intern - University of Wisconsin-Madison (May 2024 - August 2024)
+5) Data Engineer - Datacurate Technologies (June 2022 - July 2023)
+'''
